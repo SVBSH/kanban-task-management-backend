ef0623e9fd3172000a65a4523386da23e2bd4318
SVBSH <getenv@protonmail.com>
2024-09-15 17:19:11 +0200

    update user seeder to include hashing passwords

--- src/seeders/1-demo-users.js
+++ src/seeders/1-demo-users.js
@@ -1,19 +1,21 @@
 "use strict";
 
+const { hashPassword } = require("../modules/auth/auth");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("Users", [
       {
         username: "svato",
         createdAt: new Date("2024-08-09 13:26:50.052687"),
-        updatedAt: new Date("2024-08-09 13:26:50.052687"), // Adding updatedAt
-        password: "svato", // Storing passwords in plain text is insecure, use hashing in a real application
+        updatedAt: new Date("2024-08-09 13:26:50.052687"),
+        password: await hashPassword("svato"),
       },
       {
         username: "zia",
         createdAt: new Date("2024-08-09 13:27:02.57673"),
-        updatedAt: new Date("2024-08-09 13:27:02.57673"), // Adding updatedAt
-        password: "zia", // Storing passwords in plain text is insecure, use hashing in a real application
+        updatedAt: new Date("2024-08-09 13:27:02.57673"),
+        password: await hashPassword("zia"),
       },
     ]);
   },
